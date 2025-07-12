c73a395bccb502f0ebbd7aa7b4db4110ccaab68d
LazarShockX <donaldbr12@gmail.com>
2025-07-12 14:18:14 +0200

    Meetings form

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,26 +1,41 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { getQueryClient, trpc } from "@/trpc/server";
 
 import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/views/meetings-view";
+import { MeetingsListHeader } from "@/modules/meetings/ui/components/meetings-list-header";
 
-const Page = () => {
+const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+    
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(
         trpc.meetings.getMany.queryOptions({})
     )
 
     return (
-        // Dehydrate converts the server-side cache (JS objects) to a serialized string so it can be embedded in HTML and transferred to the client for hydration as JS objects can't be transferred directly
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<MeetingsViewLoading />}>
-                <ErrorBoundary fallback={<MeetingsViewError />}>
-                    <MeetingsView />
-                </ErrorBoundary>
-            </Suspense>
-        </HydrationBoundary>
+        <>
+            <MeetingsListHeader />
+            {/*Dehydrate converts the server-side cache (JS objects) to a serialized string so it can be embedded in HTML and transferred to the client for hydration as JS objects can't be transferred directly*/}
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<MeetingsViewLoading />}>
+                    <ErrorBoundary fallback={<MeetingsViewError />}>
+                        <MeetingsView />
+                    </ErrorBoundary>
+                </Suspense>
+            </HydrationBoundary>
+        </>
     )
 }
 
