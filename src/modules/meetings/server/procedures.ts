ee48b5014e5a82c368c43f257063897b5a42dc2f
LazarShockX <donaldbr12@gmail.com>
2025-07-15 17:43:36 +0200

    Hotfix: correct calculation of token's lifespan

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -29,7 +29,7 @@ export const meetingsRouter = createTRPCRouter({
         const token = streamVideo.generateUserToken({
             user_id: ctx.auth.user.id,
             exp: expirationTime,
-            validity_in_seconds: issuedAt,
+            validity_in_seconds: expirationTime - issuedAt,
         })
 
         return token;
